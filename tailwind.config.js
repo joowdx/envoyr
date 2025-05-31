5f24981616c74a7b7982caf7f1b899e46c7c1e32
jaslup16 <jaslupiba@gmail.com>
2025-05-31 15:16:17 +0800

    Update package-lock.json, refine Tailwind CSS configuration, and clean up app.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: 'class',
-    content: [
-      './resources/views/**/*.blade.php',
-      './resources/js/**/*.js',
-      './resources/js/**/*.vue',
-      './resources/components/**/*.blade.php',
-      './storage/framework/views/*.php',
-      './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-    ],
   darkMode: 'class', // Enable dark mode
+  content: [
+    './resources/views/**/*.blade.php', // Blade templates
+    './resources/js/**/*.js',          // JavaScript files
+    './resources/**/*.vue',            // Vue files
+    './resources/components/**/*.blade.php', // Blade components
+    './storage/framework/views/*.php', // Cached views
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php', // Laravel pagination views
+  ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Instrument Sans', 'ui-sans-serif', 'system-ui', 'sans-serif'], // Custom font family
+      },
       colors: {
-        primary: '#c83ebf',
-        dark: '#0f0f0f',
-        light: '#ffffff',
-        grayish: '#1f1f1f',
+        primary: '#c83ebf', // Custom primary color
+        dark: '#0f0f0f',    // Custom dark color
+        light: '#ffffff',   // Custom light color
+        grayish: '#1f1f1f', // Custom grayish color
       },
     },
   },
-  content: [
-    './resources/views/**/*.blade.php', // Include Blade templates
-    './resources/js/**/*.js',          // Include JavaScript files
-  ],
+  plugins: [],
 };
