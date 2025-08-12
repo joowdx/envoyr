00158885b0e2951cb714223d02293f013f28aabb
Darkin <lordcigelsalvar@gmail.com>
2025-08-12 09:57:16 +0800

    Refactor Transmittal model to include ULID support, define fillable fields, and establish relationships with Document, Office, Section, and User models. Update migration to create transmittals table with appropriate fields and indexing for improved performance.

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Document extends Model
 {
--- database/migrations/2025_08_11_071327_create_transmittals_table.php
+++ database/migrations/2025_08_11_071327_create_transmittals_table.php
@@ -12,8 +12,25 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('transmittals', function (Blueprint $table) {
-            $table->id();
+            $table->ulid('id')->primary();
+            $table->string('code')->unique();
+            $table->string('purpose');
+            $table->text('remarks')->nullable();
+            $table->boolean('pick_up')->default(false);
+            $table->foreignIdFor(Document::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Office::class, 'from_office_id')->constrained('offices')->cascadeOnDelete();
+            $table->foreignIdFor(Section::class, 'from_section_id')->constrained('sections')->cascadeOnDelete();
+            $table->foreignIdFor(User::class, 'from_user_id')->nullable()->constrained('users')->cascadeOnDelete();
+            $table->foreignIdFor(Office::class, 'to_office_id')->nullable()->constrained('offices')->cascadeOnDelete();
+            $table->foreignIdFor(Section::class, 'to_section_id')->nullable()->constrained('sections')->cascadeOnDelete();
+            $table->foreignIdFor(User::class, 'to_user_id')->nullable()->constrained('users')->cascadeOnDelete();
+            $table->foreignIdFor(User::class, 'liaison_id')->nullable()->constrained('users')->cascadeOnDelete();
+            $table->timestamp('received_at')->nullable();
             $table->timestamps();
+
+            $table->index(['to_office_id', 'received_at']);
+            $table->index(['document_id', 'received_at']);
+            $table->index(['from_office_id', 'created_at']);
         });
     }
 
