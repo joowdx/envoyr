dac6c6b15be9fe4450860cf32bc57d9f329155ef
Darkin <lordcigelsalvar@gmail.com>
2025-08-12 10:23:50 +0800

    Refactor Label model to extend Pivot, add ULID support, and define relationships with Document and Tag models. Update migration to create labels table with ULID as primary key and foreign key constraints.

--- app/Models/Label.php
+++ app/Models/Label.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class Label extends Model
+class Label extends Pivot
 {
-    //
+    use HasUlids;
+
+    protected $table = 'labels';
+
+    public function document(): BelongsTo
+    {
+        return $this->belongsTo(Document::class);
+    }
+
+    public function tag(): BelongsTo
+    {
+        return $this->belongsTo(Tag::class);
+    }
 }
--- database/migrations/2025_08_11_071437_create_labels_table.php
+++ database/migrations/2025_08_11_071437_create_labels_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Document;
+use App\Models\Tag;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,7 +14,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('labels', function (Blueprint $table) {
-            $table->id();
+            $table->ulid('id')->primary();
+            $table->foreignIdFor(Document::class)->constrained('documents')->cascadeOnDelete();
+            $table->foreignIdFor(Tag::class)->constrained('tags')->cascadeOnDelete();
             $table->timestamps();
         });
     }
