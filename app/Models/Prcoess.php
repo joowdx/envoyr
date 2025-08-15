bc03d5b8890379b777bfc8ce21eef2c89d0c0012
Darkin Lord Cigel <114234883+lordsalvar@users.noreply.github.com>
2025-08-15 13:03:54 +0800

    Models & Migrations (#141)

---
+++ app/Models/Prcoess.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Process extends Model
+{
+    use HasUlids;
+
+    protected $fillable = [
+        'document_id',
+        'transmittal_id',
+        'user_id',
+        'processed_at',
+        'status',
+    ];
+
+    public function document(): BelongsTo
+    {
+        return $this->belongsTo(Document::class);
+    }
+
+    public function transmittal(): BelongsTo
+    {
+        return $this->belongsTo(Transmittal::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
