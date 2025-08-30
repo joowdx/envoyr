f66ad5ed099bd8b443c034117cfa4130b27ed7a6
Ford-jpg <johnfordleoniel@gmail.com>
2025-08-30 10:23:53 +0800

    Implement authentication middleware for profile completion routes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\RegistrationController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// User invitation/registration routes with signed URLs
 Route::get('/register/{user}', [RegistrationController::class, 'show'])
     ->name('register.show')
     ->middleware(['guest', 'signed']); // Add signed middleware
@@ -14,8 +13,7 @@ Route::post('/register/{user}', [RegistrationController::class, 'store'])
     ->middleware(['guest', 'signed']); // Add signed middleware
 
 Route::get('/complete-profile', [ProfileController::class, 'showCompleteForm'])
-    ->name('complete-profile')
-    ->middleware('auth'); 
+    ->middleware(\Filament\Http\Middleware\Authenticate::class); 
 Route::post('/complete-profile', [ProfileController::class, 'storeCompleteForm'])
-    ->middleware('auth');
+    ->middleware(\Filament\Http\Middleware\Authenticate::class);
 
