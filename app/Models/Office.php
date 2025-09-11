bc225cbe81e4dad42e9ac32da98ac5cde03c2cd3
Ford-jpg <johnfordleoniel@gmail.com>
2025-09-11 10:32:00 +0800

    Add processes relationship to Classification and Office models, and update Process model to include classification_id

--- app/Models/Office.php
+++ app/Models/Office.php
@@ -33,4 +33,9 @@ class Office extends Model
     {
         return $this->hasMany(User::class);
     }
+
+    public function processes(): HasMany
+    {
+        return $this->hasMany(Process::class);
+    }
 }
